58161af907199c2ad444f36f873cb628fe42b82a
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2022-12-17 08:15:02 -0800

    Code cleanup

--- src/Dappery.Core/Infrastructure/RequestValidationBehavior.cs
+++ src/Dappery.Core/Infrastructure/RequestValidationBehavior.cs
@@ -3,13 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-
-using Microsoft.Extensions.Logging;
-
 using FluentValidation;
 using FluentValidation.Results;
-
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace Dappery.Core.Infrastructure;
 
