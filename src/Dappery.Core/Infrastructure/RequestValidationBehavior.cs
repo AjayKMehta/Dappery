4c2d098286f258d3131e16c8831a1e76098e9188
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2023-01-24 22:32:10 -0800

    :broom: Tidy using directives.

--- src/Dappery.Core/Infrastructure/RequestValidationBehavior.cs
+++ src/Dappery.Core/Infrastructure/RequestValidationBehavior.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
 using FluentValidation;
 using FluentValidation.Results;
+
 using MediatR;
-using Microsoft.Extensions.Logging;
 
 namespace Dappery.Core.Infrastructure;
 
